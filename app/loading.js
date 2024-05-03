a992877231863a9f6ddfe0299f36548f18a9c83f
Shrivatsa Upparna <142652255+shrivatsa255@users.noreply.github.com>
2024-05-03 10:04:05 +0530

    Update loading.js

--- app/loading.js
+++ app/loading.js
@@ -1,28 +1,28 @@
 // components/Loading.js
-'use client';
-import React, { useEffect, useRef } from 'react';
+// 'use client';
+// import React, { useEffect, useRef } from 'react';
 // import { useTheme } from 'next-themes';
 
 const Loading = () => {
- const container = useRef(null);
- // const { theme } = useTheme();
- // theme === 'light' ? '/loading.json' : '/imposter.json', 
+//  const container = useRef(null);
+//  // const { theme } = useTheme();
+//  // theme === 'light' ? '/loading.json' : '/imposter.json', 
 
- useEffect(() => {
-    const loadLottie = async () => {
-        const lottie = await import('lottie-web');
-        lottie.loadAnimation({
-          container: container.current, // the dom element that will contain the animation
-          renderer: 'svg',
-          loop: true,
-          autoplay: true,
-          path: '/loading.json' // the path to the animation json
-        });
-      }
-    loadLottie();
- }, []); // Add theme as a dependency to re-run the effect if the theme changes
+//  useEffect(() => {
+//     const loadLottie = async () => {
+//         const lottie = await import('lottie-web');
+//         lottie.loadAnimation({
+//           container: container.current, // the dom element that will contain the animation
+//           renderer: 'svg',
+//           loop: true,
+//           autoplay: true,
+//           path: '/loading.json' // the path to the animation json
+//         });
+//       }
+//     loadLottie();
+//  }, []); // Add theme as a dependency to re-run the effect if the theme changes
 
- return <div className="justify-center items-center w-screen h-screen" ref={container}></div>;
+ return <div className="justify-center items-center w-screen h-screen" >Loading....</div>;
 };
 
 export default Loading;
